228da1d67b3dcb4655ebf54785586a1097e1196c
Rid <rid@cylo.io>
2022-02-07 11:12:27 +0000

    🚀 Make compatible
    Signed-off-by: Rid <rid@cylo.io>

---
+++ src/packages/ganache/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f /etc/installer_completed ]; then
+    until [[ $(curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/${INSTANCE_ID}" | grep ' 200 ') ]]
+        do
+        sleep 5
+    done
+    touch /etc/installer_completed
+fi
+
+exec "node", "/app/dist/node/cli.js", "-h", "0.0.0.0", "-p", "${PORT}", "--account_keys_path", "/appstore/keys.json", "-a", "1000", "-s", "${SEED}", "-i", "1337"
